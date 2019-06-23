3af010f56563d34e736651eb98c0d49e2114bbfe
icemanx7 <richard.thomas36@gmail.com>
2019-06-23 21:20:51 +0200

    added the stream code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.stream.Stream;
+
 public class Main {
 
+    public static void order(String words) {
+        String[] seperatedWords = words.split(" ");
+        Stream.of(seperatedWords).forEach(i -> System.out.println(i));
+        System.out.println(seperatedWords);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        order("is2 Thi1s T4est 3a");
     }
 }
